0948fda2bfe0fa32ca84e1dcb06e3d395a7f866c
Ben Gardiner <ben.l.gardiner@gmail.com>
2019-05-09 23:43:13 -0400

    I made a custom aglio theme instead -- issue #142

--- generateme.sh
+++ generateme.sh
@@ -6,12 +6,7 @@ PDF_FILE=otapi.pdf
 
 CHROME='/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'
 
-aglio -t otto --theme-full-width -i "${APIB_FILE}" -o "${HTML_FILE}"
-# for default theme
-#sed -e 's/collapse-button"/collapse-button show"/g' -i'' "${HTML_FILE}"
-# for otto them
-sed -e 's/class="collapse-content"/class="collapse-content" style="display: inline;"/g' -i'' "${HTML_FILE}"
-
+aglio -t olio-printing --theme-full-width -i "${APIB_FILE}" -o "${HTML_FILE}"
 
 # not working yet
 #"${CHROME}" --headless --disable-gpu --hide-scrollbars --print-to-pdf "${HTML_FILE}"
